e382aff686cc75ef9de57dd304e0b5d72af55330
Michael Bommarito <mike@lexpredict.com>
2017-07-29 22:31:38 -0400

    Adding medical source data retrieval

---
+++ en/medical/download_icd_2017.sh
@@ -0,0 +1,5 @@
+wget https://www.cms.gov/Medicare/Coding/ICD10/Downloads/2017-ICD10-Code-Tables-Index.zip
+wget https://www.cms.gov/Medicare/Coding/ICD10/Downloads/2017-ICD10-Code-Descriptions.zip
+wget https://www.cms.gov/Medicare/Coding/ICD10/Downloads/2017-PCS-Long-Abbrev-Titles.zip
+wget https://www.cms.gov/Medicare/Coding/ICD10/Downloads/2017-New-Procedure-Codes.zip
+wget https://www.cms.gov/Medicare/Coding/ICD10/Downloads/2017-PCS-Code-Tables.zip
